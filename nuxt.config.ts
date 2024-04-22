b7fa24f5db0f31f547d82e99973a9f1deeaf6f3d
Kwiky93 <sei93@yandex.ru>
2024-04-22 14:53:37 +0300

    Added configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  app: {
+    baseURL: "/test-exercise/",
+  },
   devtools: { enabled: true },
   modules: ["vuetify-nuxt-module", "@pinia/nuxt"],
 });
