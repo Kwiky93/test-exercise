8442e6fe198bf0d1b506dc908cd65f0e93e41f04
Kwiky93 <sei93@yandex.ru>
2024-04-22 15:02:49 +0300

    Updates

--- nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  ssr: false,
-  app: {
-    baseURL: "/test-exercise/",
-  },
-  devtools: { enabled: true },
-  modules: ["vuetify-nuxt-module", "@pinia/nuxt"],
-});
--- stores/index.ts
+++
@@ -1,7 +0,0 @@
-import { defineStore } from "pinia";
-
-export const useWebsiteStore = defineStore("websiteStore", {
-  state: () => ({}),
-
-  actions: {},
-});
